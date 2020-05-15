f18a13cb72e8e85a50f1a72b771126f5185d3473
LFCRab <56073739+LFCRab@users.noreply.github.com>
2020-05-15 12:44:24 +0100

    Removed timestamp

--- index.js
+++ index.js
@@ -75,7 +75,6 @@ bot.on('message', msg => {
         .setDescription("The bot that'll read you a word a day!")
         .setThumbnail('https://i.pinimg.com/736x/3c/88/c8/3c88c8510028f627cf58792795629ed1.jpg')
         .setImage('https://cdn.discordapp.com/attachments/571058265404997657/707298692843176077/Bilbo_Rabbins.jpg')
-        .setTimestamp()
         .setFooter('Have fun with it!', 'https://i.pinimg.com/736x/3c/88/c8/3c88c8510028f627cf58792795629ed1.jpg');
 
     // Refers to the user requesting the response.
