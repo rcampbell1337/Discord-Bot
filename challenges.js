3dda431a34305bb1e8c78d62a387609ae0b0db56
robbie <robbielcampbell@hotmail.com>
2021-01-07 19:41:30 +0000

    :sparkles: random coding challenges :sparkles:

--- challenges.js
+++ challenges.js
@@ -1,13 +1,20 @@
 module.exports =
-titles = [
-    "Return the Sum of Two Numbers",
-    "Return the Next Number from the Integer Passed",
-    "Convert Minutes into Seconds",
-    "Area of a Triangle",
-    "Convert Hours into Seconds",
-    "Fix Basic Calculator",
-    "Maximum Edge of a Triangle"
-]
+    titles = [
+        "Return the Sum of Two Numbers",
+        "Return the Next Number from the Integer Passed",
+        "Convert Minutes into Seconds",
+        "Area of a Triangle",
+        "Convert Hours into Seconds",
+        "Fix Basic Calculator",
+        "Maximum Edge of a Triangle",
+        "Find the Perimeter of a Rectangle",
+        "Drunken Python",
+        "Face Interval",
+        "Stuttering Function",
+        "Xs and Os, Nobody Knows",
+        "Vowel Replacer",
+        "Hiding the Card Number"
+    ]
 
 description = [
     "Create a function that takes two numbers as arguments and return their sum.",
@@ -16,5 +23,12 @@ description = [
     "Write a function that takes the base and height of a triangle and return its area.",
     "Write a function that converts hours into seconds.",
     "Mubashir created a function that takes two numbers a and b and an operator o. Function should return the result of the corresponding mathematical function on both numbers. If the operator is is not one of the specified characters +, -, /, * the function should return None. Help him by fixing the code in the code tab to pass this challenge.",
-    "Create a function that finds the maximum range of a triangle's third edge, where the side lengths are all integers."
+    "Create a function that finds the maximum range of a triangle's third edge, where the side lengths are all integers.",
+    "Create a function that takes length and width and finds the perimeter of a rectangle.",
+    "Python got drunk and the built-in functions str() and int() are acting odd... \n You need to create two functions to substitute str() and int(). A function called int_to_str() that converts integers into strings and a function called str_to_int() that converts strings into integers.",
+    'In mathematics, interval is the difference between the largest number and the smallest number in a list. \nTo illustrate:\nA = (3, 5, 7, 23, 11, 42, 80)\nInterval of A = 80 - 3 = 77\nCreate a function that takes a list and returns ":)" if the interval of the list is equal to any other element; otherwise return ":(".',
+    "Write a function that stutters a word as if someone is struggling to read it. The first two letters are repeated twice with an ellipsis ... and space after each, and then the word is pronounced with a question mark ?.",
+    'Create a function that takes a string, checks if it has the same number of "x"s and "o"s and returns either True or False.',
+    "Create a function that replaces all the vowels in a string with a specified character.",
+    "Write a function that takes a credit card number and only displays the last four characters. The rest of the card number must be replaced by ************."
 ]
